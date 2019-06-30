b87116ce4f6888ab35a5e18415e74b4d736ae515
mqduck <mqduck@mqduck.net>
2019-06-30 00:03:09 -0700

    Add matrix_to_string().

--- leetnode/__init__.py
+++ leetnode/__init__.py
@@ -32,7 +32,6 @@ def construct_binary_tree(arr: List) -> Optional[TreeNode]:
 
     root = TreeNode(arr[0])
     construct(0, root)
-
     return root
 
 
@@ -83,9 +82,26 @@ def construct_linked_list(arr: List) -> Optional[ListNode]:
     return head
 
 
-if __name__ == "__main__":
+def matrix_to_string(matrix: List[List]) -> str:
+    if not isinstance(matrix, List) or len(matrix) == 0 or not isinstance(matrix[0], List):
+        raise TypeError
+
+    lengths = [len(str(item)) for row in matrix for item in row]
+    item_spacing = 0 if len(lengths) == 0 else max(lengths)
+    del lengths
+
+    str_arr = []
+    for i in range(len(matrix)):
+        str_arr.append('[ [ ' if i == 0 else '  [ ')
+        str_arr.append(''.join([f'{item:>{item_spacing}}, ' for item in matrix[i]])[:-2] + ' ]')
+        str_arr.append(' ]' if i == len(matrix) - 1 else ',\n')
+    return ''.join(str_arr)
+
+
+if __name__ == '__main__':
     llist = construct_linked_list([0, 1, 2, 3, 4, 5])
     btree = construct_binary_tree([3, 9, 20, None, None, 15, 7])
     a = flatten_binary_tree(btree)
     print(list(llist))
     print(list(btree))
+    print(matrix_to_string([[3, 0, 8, 4], [2, 4, 5, 7], [9, 2, 6, 3], [0, 3, 1, 2340]]))
